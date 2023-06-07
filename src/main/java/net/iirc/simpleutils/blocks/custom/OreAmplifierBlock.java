270038626bcbcf2f7e9aa920cffa7f3529183772
andrew3467 <apgraser2@gmail.com>
2023-06-07 13:45:19 -0400

    Ore Amplifier Block Rotate Depending On Placement

--- src/main/java/net/iirc/simpleutils/blocks/custom/OreAmplifierBlock.java
+++ src/main/java/net/iirc/simpleutils/blocks/custom/OreAmplifierBlock.java
@@ -4,17 +4,22 @@ import net.iirc.simpleutils.blocks.entity.ModBlockEntities;
 import net.iirc.simpleutils.blocks.entity.OreAmplifierBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.gameevent.GameEventListener;
 import org.jetbrains.annotations.Nullable;
 
 public class OreAmplifierBlock extends BaseEntityBlock {
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+
     public OreAmplifierBlock(Properties pProperties) {
         super(pProperties);
     }
@@ -42,5 +47,36 @@ public class OreAmplifierBlock extends BaseEntityBlock {
     @Override
     public RenderShape getRenderShape(BlockState pState) {
         return RenderShape.MODEL;
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        return this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    public BlockState rotate(BlockState pState, Rotation pRotation) {
+        return pState.setValue(FACING, pRotation.rotate(pState.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState pState, Mirror pMirror) {
+        return pState.rotate(pMirror.getRotation(pState.getValue(FACING)));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving) {
+        if (pState.getBlock() != pNewState.getBlock()) {
+            BlockEntity blockEntity = pLevel.getBlockEntity(pPos);
+            if (blockEntity instanceof OreAmplifierBlockEntity) {
+                ((OreAmplifierBlockEntity) blockEntity).drops();
+            }
+        }
+        super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
     }
 }
--- src/main/java/net/iirc/simpleutils/blocks/entity/OreAmplifierBlockEntity.java
+++ src/main/java/net/iirc/simpleutils/blocks/entity/OreAmplifierBlockEntity.java
@@ -1,26 +1,59 @@
 package net.iirc.simpleutils.blocks.entity;
 
+import net.iirc.simpleutils.blocks.custom.OreAmplifierBlock;
+import net.iirc.simpleutils.networking.ModMessages;
+import net.iirc.simpleutils.networking.packet.ItemStackSyncS2CPacket;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.Containers;
 import net.minecraft.world.MenuProvider;
+import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Map;
+
 public class OreAmplifierBlockEntity extends BlockEntity implements MenuProvider {
+    private final ItemStackHandler itemHandler = new ItemStackHandler(3){
+        @Override
+        protected void onContentsChanged(int slot) {
+            setChanged();
+
+            if(!level.isClientSide()){
+                ModMessages.sendToClients(new ItemStackSyncS2CPacket(this, worldPosition));
+            }
+        }
+    };
 
 
+    private LazyOptional<IItemHandler> lazyItemHandler = LazyOptional.empty();
 
+
+    private int progress = 0;
+    private int maxProgress = 80;
+
+    private final Map<Direction, LazyOptional<WrappedHandler>> directionWrappedHandlerMap =
+            Map.of(Direction.DOWN, LazyOptional.of(() -> new WrappedHandler(itemHandler, (i) -> i == 2, (i, s) -> false)),
+                    Direction.NORTH, LazyOptional.of(() -> new WrappedHandler(itemHandler, (index) -> index == 1,
+                            (index, stack) -> itemHandler.isItemValid(1, stack))),
+                    Direction.SOUTH, LazyOptional.of(() -> new WrappedHandler(itemHandler, (i) -> i == 2, (i, s) -> false)),
+                    Direction.EAST, LazyOptional.of(() -> new WrappedHandler(itemHandler, (i) -> i == 1,
+                            (index, stack) -> itemHandler.isItemValid(1, stack))),
+                    Direction.WEST, LazyOptional.of(() -> new WrappedHandler(itemHandler, (index) -> index == 0 || index == 1,
+                            (index, stack) -> itemHandler.isItemValid(0, stack) || itemHandler.isItemValid(1, stack))));
 
     public OreAmplifierBlockEntity(BlockPos pPos, BlockState pBlockState) {
         super(ModBlockEntities.ORE_AMPLIFIER.get(), pPos, pBlockState);
@@ -29,11 +62,22 @@ public class OreAmplifierBlockEntity extends BlockEntity implements MenuProvider
     @Override
     public void onLoad() {
         super.onLoad();
+        lazyItemHandler = LazyOptional.of(() -> itemHandler);
+    }
+
+    public void drops() {
+        SimpleContainer inventory = new SimpleContainer(itemHandler.getSlots());
+        for (int i = 0; i < itemHandler.getSlots(); i++) {
+            inventory.setItem(i, itemHandler.getStackInSlot(i));
+        }
+
+        Containers.dropContents(this.level, this.worldPosition, inventory);
     }
 
     @Override
     public void invalidateCaps() {
         super.invalidateCaps();
+        lazyItemHandler.invalidate();
     }
 
     @Override
@@ -48,9 +92,17 @@ public class OreAmplifierBlockEntity extends BlockEntity implements MenuProvider
 
 
     public static void tick(Level level, BlockPos pos, BlockState state, OreAmplifierBlockEntity pEntity){
+        if(level.isClientSide()) {
+            return;
+        }
 
+        //Check for recipe, Check for fuel
+        //Increase progress by 1, update state
     }
 
+    private void resetProgress() {
+        this.progress = 0;
+    }
 
 
     @Override
@@ -66,8 +118,33 @@ public class OreAmplifierBlockEntity extends BlockEntity implements MenuProvider
 
     @Override
     public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
+        if(cap == ForgeCapabilities.ITEM_HANDLER) {
+            if(side == null){
+                return lazyItemHandler.cast();
+            }
 
+            if(directionWrappedHandlerMap.containsKey(side)){
+                Direction localDir = this.getBlockState().getValue(OreAmplifierBlock.FACING);
+
+                if(side == Direction.UP || side == Direction.DOWN){
+                    return directionWrappedHandlerMap.get(side).cast();
+                }
+
+                return switch(localDir){
+                    default -> directionWrappedHandlerMap.get(side.getOpposite()).cast();
+                    case EAST -> directionWrappedHandlerMap.get(side.getClockWise()).cast();
+                    case SOUTH -> directionWrappedHandlerMap.get(side).cast();
+                    case WEST -> directionWrappedHandlerMap.get(side.getCounterClockWise()).cast();
+                };
+            }
+        }
 
         return super.getCapability(cap, side);
+    }
+
+    public void setHandler(ItemStackHandler itemStackHandler) {
+        for (int i = 0; i < itemStackHandler.getSlots(); i++) {
+            itemHandler.setStackInSlot(i, itemStackHandler.getStackInSlot(i));
+        }
     }
 }
