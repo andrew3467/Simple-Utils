ee2fe8d6f89cc9301d1bbe8fa2d59d870a0d62f0
WolfyFX <php115145@gmail.com>
2023-06-08 18:14:43 +0200

    Update OreAmplifierBlock.java

--- src/main/java/net/iirc/simpleutils/blocks/custom/OreAmplifierBlock.java
+++ src/main/java/net/iirc/simpleutils/blocks/custom/OreAmplifierBlock.java
@@ -4,6 +4,10 @@ import net.iirc.simpleutils.blocks.entity.ModBlockEntities;
 import net.iirc.simpleutils.blocks.entity.OreAmplifierBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
@@ -15,6 +19,8 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.gameevent.GameEventListener;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
 
 public class OreAmplifierBlock extends BaseEntityBlock {
@@ -23,6 +29,7 @@ public class OreAmplifierBlock extends BaseEntityBlock {
     public OreAmplifierBlock(Properties pProperties) {
         super(pProperties);
     }
+
 
 
     @Nullable
@@ -67,6 +74,21 @@ public class OreAmplifierBlock extends BaseEntityBlock {
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         builder.add(FACING);
+    }
+
+    @Override
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos,
+                                 Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+        if (!pLevel.isClientSide()) {
+            BlockEntity entity = pLevel.getBlockEntity(pPos);
+            if(entity instanceof OreAmplifierBlockEntity) {
+                NetworkHooks.openScreen(((ServerPlayer)pPlayer), (OreAmplifierBlockEntity)entity, pPos);
+            } else {
+                throw new IllegalStateException("Container Provider missing.");
+            }
+        }
+
+        return InteractionResult.sidedSuccess(pLevel.isClientSide());
     }
 
     @Override
