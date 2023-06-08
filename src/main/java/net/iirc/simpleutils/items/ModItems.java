25236ebd074223e59fb8e7219b29c77df7f9939a
WolfyFX <php115145@gmail.com>
2023-06-08 21:16:52 +0200

    ingot

--- src/main/java/net/iirc/simpleutils/items/ModItems.java
+++ src/main/java/net/iirc/simpleutils/items/ModItems.java
@@ -3,6 +3,7 @@ package net.iirc.simpleutils.items;
 import net.iirc.simpleutils.Fluid.ModFluids;
 import net.iirc.simpleutils.SimpleUtils;
 import net.minecraft.world.item.BucketItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
@@ -18,6 +19,12 @@ public class ModItems {
     public static final RegistryObject<Item> SOAP_WATER_BUCKET = ITEMS.register("soap_water_bucket",
             () -> new BucketItem(ModFluids.SOURCE_SOAP_WATER,
                     new Item.Properties().tab(ModCreativeModeTab.SIMPLE_UTILS_TAB).craftRemainder(Items.BUCKET).stacksTo(1)));
+
+    public static final RegistryObject<Item> AURORA_INGOT = ITEMS.register("aurora_ingot",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SIMPLE_UTILS_TAB)));
+
+    public static final RegistryObject<Item> WOLFRAMITE = ITEMS.register("wolframite",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SIMPLE_UTILS_TAB)));
 
 
     public static void register(IEventBus eventBus){
